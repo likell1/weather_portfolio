642ca28c209a707de8d2c348959ad7caf23ca589
hyeokjun <eru010930@gmail.com>
2022-08-22 01:56:02 +0900

    [Feat] API, HTML 연동

--- weather_portfolio.py
+++ weather_portfolio.py
@@ -1,28 +1,58 @@
-from mmap import PROT_READ
+import googletrans
 import requests
 import json
 
+translate = googletrans.Translator()
 key = "f0f2de8a8e49d99a77fb762cab177c25"
 k2C = lambda k: k - 273.15
 
-while True:
 
-    user_city = input("날씨 정보를 원하는 지역을 입력해주세요 : ")
-
+def total_data():
+    user_city = input()
+    user_city = translate.translate(user_city, dest="en").text
+    print(user_city)
+    
     if user_city == "":
         print("공백은 인식할 수 없습니다.")
 
-    api = f"http://api.openweathermap.org/data/2.5/weather?q={user_city}&APPID={key}"
+    api = f"http://api.openweathermap.org/data/2.5/forecast?q={user_city}&APPID={key}"
     result = requests.get(api)
 
     if result.status_code == 200:
         data = json.loads(result.text)
-        temp = round(k2C(data["main"]["temp"]),1)
-        weather = data["weather"][0]["main"]
-        print(data["weather"])
-        print(user_city,"의 온도는", temp, "°C")
-        print(user_city,"의 날씨는", weather, "입니다.")
-        exit()
+    
+        forecast_data = []
+        
+        for item in data["list"]:
+            
+            date = item["dt_txt"]
+            temp = round(k2C(item["main"]["temp"]), 1)
+            temp_min = round(k2C(item["main"]["temp_min"]), 1)
+            temp_max = round(k2C(item["main"]["temp_max"]), 1)
+            feels_like = round(k2C(item["main"]["feels_like"]), 1)
+            humidity = item["main"]["humidity"]
+            weather = item["weather"][0]["main"]
+            weather_description = item["weather"][0]["description"]
+            
+            try:
+                rain = item["rain"]
+                forecast_data_sub = {
+                    "date":date,
+                    "temp":temp,
+                    "temp_min":temp_min,
+                    "temp_max":temp_max,
+                    "feels_like":feels_like,
+                    "humidity":humidity,
+                    "weather":weather,
+                    "weather_description":weather_description,
+                    "rain": rain
+                }
+                forecast_data.append(forecast_data_sub) 
+            except:
+                pass
+                    
+        return forecast_data
+
     else:
         print("다시 입력해주세요")
 
